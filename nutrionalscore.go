ee8e30d1228f5adeafe5dce33e6eb07fb50e4d4b
Cecilia Quintana <20249591+ceciquin@users.noreply.github.com>
2023-01-16 21:53:31 -0300

    Adding Nutritional Data structs

--- nutrionalscore.go
+++ nutrionalscore.go
@@ -1,2 +1,41 @@
 package main
 
+type ScoreType int
+
+const (
+	Food ScoreType = iota
+	Beverage
+	Water
+	Cheese
+)
+
+type NutritionalScore struct{
+	Value int 
+	Positive int
+	Negative int
+	ScoreType ScoreType
+}
+
+type EnergyKJ float64
+
+type SugarGram float64
+
+type SaturatedFattyAcids float64
+
+type SodiumMilligram float64
+
+type FruitsPercent float64
+
+type FiberGram float64
+
+type ProteinGram float64
+
+type NutrionalData struct {
+	Energy  			EnergyKJ
+	Sugars  			SugarGram
+	SaturatedFattyAcids SaturatedFattyAcids
+	Sodium 				SodiumMilligram
+	Fruits 				FruitsPercent
+	ProteinGram 		ProteinGram
+	IsWater 			bool
+}
