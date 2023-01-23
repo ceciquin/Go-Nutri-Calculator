c62a3003bb33af3aee398427ac3b246c6c57ffb4
Cecilia Quintana <20249591+ceciquin@users.noreply.github.com>
2023-01-23 17:06:13 -0300

    finished the scientific logic for the nutrional calculator. added method and function tu evaluate final score taking into account the type of the food.

--- main.go
+++ main.go
@@ -8,14 +8,15 @@ import(
 func main(){
 
 	ns := GetNutrionalScore(NutrionalData{
-		Energy: EnergyFromKcal(),
-		Sugars: SugarGram(),
-		SaturatedFattyAcids: SaturatedFattyAcids(),
-		Sodium: SodiumMilligram(),
-		Fruits: FruitsPercent(),
-		Fibre: FibreGram(),
-		Protein: ProteinGram(),
+		Energy: EnergyFromKcal(500),
+		Sugars: SugarGram(20),
+		SaturatedFattyAcids: SaturatedFattyAcids(2),
+		Sodium: SodiumMilligram(700),
+		Fruits: FruitsPercent(50),
+		Fibre: FibreGram(6),
+		Protein: ProteinGram(10),
 	}, Food)
 
 	fmt.Printf("Nutritional score: %d\n", ns.Value)
+	fmt.Printf("NutriScore: %s\n", ns.GetNutriScore())
 }
